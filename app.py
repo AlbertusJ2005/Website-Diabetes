c59e78a10be6c33b3345c5eec7f2f893e9f21afc
AlbertusJ2005 <albertusj2005@gmail.com>
2026-04-28 15:17:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,8 @@ def predict():
         'probability': round(probability * 100, 2)  # persen, dibulatkan 2 angka
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
